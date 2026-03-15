84b1a9a6cac021e08830a9098daecc6b5bc5a85a
Taha Zahid <tz09220@st.habib.edu.pk>
2026-03-15 19:51:15 +0500

    Added visualization

--- main.cpp
+++ main.cpp
@@ -24,11 +24,8 @@
 #include "class/visualize.h"
 #include "class/reconstruction.h"
 #include "class/feature_matching.h"			
-#include "class/ranking_system.h"
 #include "class/genetic_algorithm.h"
 // test
-#define TOP_k 5
-#define BRANCH_b 3
 
 //#define NO_RIM_INFO
 #define NO_BASE_INFO
@@ -60,10 +57,6 @@ int main(int argc, char** argv)
 	double calculation_time(0);
 	int s_time(0), e_time(0);
 
-	int step_size = shard.size(); 
-	if (argv[1] != NULL) {
-		step_size = std::atoi(argv[1]);
-	}
 	//#################### PCL viewer setting ####################//
 	viewer->setBackgroundColor(0, 0, 0);
 	viewer->addCoordinateSystem(1.0);
@@ -254,366 +247,29 @@ int main(int argc, char** argv)
 	std::system(ga_mkdir_cmd.c_str());
 	SaveAcc(path_result_ga, sherd_acc, edge_acc, time_ga);
 
-	list<LCSIndex>::iterator iter = LCS_out.begin();
-	cout << "Total number pruned : " << LCS_out.size() << endl;
-	int count_move_state(0);
-
-	// cout << "#################### Genetic Algorithm search ####################" << endl;
-	//////#################### Incremental graph building ####################//
-	StateManager manager(TOP_k, BRANCH_b, shard, LCS_out, step_size, path + "Result");
-	manager.BuildStep();
-
-	if (!manager.out_state_.empty()) {
-		const State& top_state = manager.out_state_[0];
-
-		//#################### BAISER (before fine) ####################//
-		MatrixXd graph_baiser_pre = MatrixXd::Zero(SHARD_NUMBER, SHARD_NUMBER);
-		for (size_t g = 0; g < top_state.graph_.size(); ++g) {
-			for (int i = 0; i < SHARD_NUMBER; ++i) {
-				for (int j = 0; j < SHARD_NUMBER; ++j) {
-					if (top_state.graph_[g].simple_graph_(i, j)) {
-						graph_baiser_pre(i, j) = 1;
-					}
-				}
-			}
-		}
-
-		vector<Trans> T_baiser_pre = T_axis;
-		for (int i = 0; i < SHARD_NUMBER; ++i) {
-			if (!shard_on_off[i])
-				continue;
-			for (size_t g = 0; g < top_state.graph_.size(); ++g) {
-				if (top_state.graph_[g].node_[i]) {
-					Matrix4d T_obj = Matrix4d::Identity();
-					top_state.graph_[g].T_[i].Output(T_obj);
-					T_baiser_pre[i].Input(T_obj);
-					break;
-				}
-			}
-		}
-
-		vector<bool> right_sherd_baiser_pre(SHARD_NUMBER, true);
-		for (int i = 0; i < SHARD_NUMBER; i++) {
-			if (!shard_on_off[i])
-				right_sherd_baiser_pre[i] = false;
-		}
-
-		auto [k_sherd_baiser_pre, t_sherd_baiser_pre, k_edge_baiser_pre, t_edge_baiser_pre] = CountResult(
-			GT_graph, GT_trans, graph_baiser_pre, T_baiser_pre, right_sherd_baiser_pre);
-
-		//#################### GA (after fine) ####################//
-		vector<Geom> shard_ga_fine = shard;
-		vector<Matrix3d> R_ga_fine(SHARD_NUMBER, Matrix3d::Identity());
-		vector<Vector3d> t_ga_fine(SHARD_NUMBER, Vector3d::Zero());
-		vector<bool> true_node_ga(SHARD_NUMBER, false);
-		for (int i = 0; i < SHARD_NUMBER; ++i) {
-			if (!shard_on_off[i])
-				continue;
-			true_node_ga[i] = true;
-			Matrix3d R = Matrix3d::Identity();
-			Vector3d t = Vector3d::Zero();
-			T_ga[i].Output(R, t);
-			shard_ga_fine[i].Move(R, t, true);
-			R_ga_fine[i] = R;
-			t_ga_fine[i] = t;
-		}
-		MatrixXd graph_ga_post = graph_ga;
-		IcpFine(shard_ga_fine, R_ga_fine, t_ga_fine, true_node_ga, graph_ga_post);
-
-		vector<Trans> T_ga_post = T_axis;
-		for (int i = 0; i < SHARD_NUMBER; ++i) {
-			if (!true_node_ga[i])
-				continue;
-			T_ga_post[i].Input(R_ga_fine[i], t_ga_fine[i]);
-		}
-		vector<bool> right_sherd_ga_post(SHARD_NUMBER, true);
-		for (int i = 0; i < SHARD_NUMBER; i++) {
-			if (!shard_on_off[i])
-				right_sherd_ga_post[i] = false;
-		}
-		auto [k_sherd_ga_post, t_sherd_ga_post, k_edge_ga_post, t_edge_ga_post] = CountResult(
-			GT_graph, GT_trans, graph_ga_post, T_ga_post, right_sherd_ga_post);
-
-		//#################### BAISER (after fine) ####################//
-		vector<Geom> shard_baiser_fine = shard;
-		vector<Matrix3d> R_baiser_fine(SHARD_NUMBER, Matrix3d::Identity());
-		vector<Vector3d> t_baiser_fine(SHARD_NUMBER, Vector3d::Zero());
-		vector<bool> true_node_baiser = top_state.true_node_;
-
-		for (int i = 0; i < SHARD_NUMBER; ++i) {
-			if (!true_node_baiser[i])
-				continue;
-			for (size_t g = 0; g < top_state.graph_.size(); ++g) {
-				if (top_state.graph_[g].node_[i]) {
-					Matrix3d R = Matrix3d::Identity();
-					Vector3d t = Vector3d::Zero();
-					top_state.graph_[g].T_[i].Output(R, t);
-					shard_baiser_fine[i].Move(R, t, true);
-					R_baiser_fine[i] = R;
-					t_baiser_fine[i] = t;
-					break;
-				}
-			}
-		}
-
-		MatrixXd graph_baiser_post = graph_baiser_pre;
-		IcpFine(shard_baiser_fine, R_baiser_fine, t_baiser_fine, true_node_baiser, graph_baiser_post);
-
-		vector<Trans> T_baiser_post = T_axis;
-		for (int i = 0; i < SHARD_NUMBER; ++i) {
-			if (!true_node_baiser[i])
-				continue;
-			T_baiser_post[i].Input(R_baiser_fine[i], t_baiser_fine[i]);
-		}
-		vector<bool> right_sherd_baiser_post(SHARD_NUMBER, true);
-		for (int i = 0; i < SHARD_NUMBER; i++) {
-			if (!shard_on_off[i])
-				right_sherd_baiser_post[i] = false;
-		}
-		auto [k_sherd_baiser_post, t_sherd_baiser_post, k_edge_baiser_post, t_edge_baiser_post] = CountResult(
-			GT_graph, GT_trans, graph_baiser_post, T_baiser_post, right_sherd_baiser_post);
-
-		cout << "########## GA vs BAISER Comparison ##########" << endl;
-		cout << "[Before Fine] GA     Sherd " << k_sherd << "/" << t_sherd
-			<< ", Edge " << k_edge << "/" << t_edge << endl;
-		cout << "[Before Fine] BAISER Sherd " << k_sherd_baiser_pre << "/" << t_sherd_baiser_pre
-			<< ", Edge " << k_edge_baiser_pre << "/" << t_edge_baiser_pre << endl;
-		cout << "[After Fine ] GA     Sherd " << k_sherd_ga_post << "/" << t_sherd_ga_post
-			<< ", Edge " << k_edge_ga_post << "/" << t_edge_ga_post << endl;
-		cout << "[After Fine ] BAISER Sherd " << k_sherd_baiser_post << "/" << t_sherd_baiser_post
-			<< ", Edge " << k_edge_baiser_post << "/" << t_edge_baiser_post << endl;
-		cout << "#############################################" << endl;
-	}
-
-	int num_total = manager.out_state_.size();
-
-	vector<Visualize> pc_overlap(num_total);
-
-	e_time = clock();
-	double time_result = (e_time - s_time) / 1000;
-	cout << "Time after data load : " << time_result << "sec" << endl;
-
-	if (manager.out_state_.size() > 1) {
-		if (manager.out_state_[0].state_score_ == manager.out_state_[1].state_score_)
-			cout << "!!!!!!!!!! There is co-first ranked results check both results !!!!!!!!!!" << endl;
-	}
-
-	bool is_whole_shards_used = false;
-	count_move_state = 0;
-	iter = LCS_out.begin();
-	bool is_first_state = false;
-	vector<Trans> T_fine(SHARD_NUMBER);
-	vector<bool> right_sherd(SHARD_NUMBER, true);
 	for (int i = 0; i < SHARD_NUMBER; i++) {
-		if (!shard_on_off[i])
-			right_sherd[i] = false;
+		pc_origin[i].TurnOffData(viewer);
 	}
+
+	// Apply GA transforms and show result
+	for (int i = 0; i < SHARD_NUMBER; i++) {
+		if (!shard_on_off[i]) continue;
+		Matrix3d R_vis = Matrix3d::Identity();
+		Vector3d t_vis = Vector3d::Zero();
+		T_ga[i].Output(R_vis, t_vis);
+		pc_origin[i].UpdateData(viewer,
+			shard[i].edge_line_.point_,
+			shard[i].edge_line_.normal_);
+		pc_origin[i].MeshTransform(R_vis, t_vis, viewer);
+		pc_origin[i].AddPointCloud(viewer);
+		pc_origin[i].AddMesh(viewer);
+	}
+	viewer->resetCamera();
+
+	// Simple viewer loop — press q to quit
+	cout << "Showing GA assembly result. Press Q to quit." << endl;
 	while (!viewer->wasStopped()) {
-		viewer->spinOnce(5);
-		if (vis.first_) {
-			for (int i = 0; i < SHARD_NUMBER; i++) {
-				pc_origin[i].TurnOffData(viewer);
-				if (shard_on_off[i])
-					right_sherd[i] = true;
-				else
-					right_sherd[i] = false;
-			}
-			if (count_move_state != 0) {
-				if (vis.is_fine_) {
-					ReverseFine(viewer, pc_origin, manager, count_move_state, T_fine);
-					vis.is_fine_ = false;
-				}
-				else 
-					TurnoffandReturn(viewer, pc_origin, manager, count_move_state);
-				
-				pc_overlap[count_move_state].TurnOffData(viewer);
-				count_move_state = 0;
-				cout << "Go to first result" << endl;
-			}
-			if (count_move_state == 0 && !is_first_state) {
-				VisCurrentState(viewer, pc_origin, manager, count_move_state, pc_overlap, T_axis);
-				pc_overlap[count_move_state].AddPointCloud(viewer, 255, 0, 0);
-				is_first_state = true;
-			}
-			vis.first_ = false;
-		}
-
-		else if (vis.right_) {
-			is_first_state = false;
-			if (count_move_state >= num_total - 1) {
-				cout << "There is no more result. Please press 'spacebar' to go first result" << endl;
-			}
-			else {
-				for (int i = 0; i < SHARD_NUMBER; i++) {
-					if (shard_on_off[i])
-						right_sherd[i] = true;
-					else
-						right_sherd[i] = false;
-				}
-				if (vis.is_fine_) {
-					ReverseFine(viewer, pc_origin, manager, count_move_state, T_fine);
-					vis.is_fine_ = false;
-				}
-				else
-					TurnoffandReturn(viewer, pc_origin, manager, count_move_state);
-
-				pc_overlap[count_move_state].TurnOffData(viewer);
-				count_move_state++;
-				VisCurrentState(viewer, pc_origin, manager, count_move_state, pc_overlap, T_axis);
-
-				pc_overlap[count_move_state].AddPointCloud(viewer, 255, 0, 0);
-			}
-			vis.right_ = false;
-		}
-
-		else if (vis.left_) {
-			if (count_move_state < 1) {
-				cout << "This is first result" << endl;
-				is_first_state = true;
-			}
-			else {
-				for (int i = 0; i < SHARD_NUMBER; i++) {
-					if (shard_on_off[i])
-						right_sherd[i] = true;
-					else
-						right_sherd[i] = false;
-				}
-				if (vis.is_fine_) {
-					ReverseFine(viewer, pc_origin, manager, count_move_state, T_fine);
-					vis.is_fine_ = false;
-				}
-				else
-					TurnoffandReturn(viewer, pc_origin, manager, count_move_state);
-
-				pc_overlap[count_move_state].TurnOffData(viewer);
-				count_move_state--;
-				VisCurrentState(viewer, pc_origin, manager, count_move_state, pc_overlap, T_axis);
-
-				pc_overlap[count_move_state].AddPointCloud(viewer, 255, 0, 0);
-			}
-			vis.left_ = false;
-		}
-
-		else if (vis.obj_) {
-			ObjVisualize(viewer, 
-				pc_origin, 
-				manager, 
-				right_sherd, 
-				count_move_state);
-			vis.obj_ = false;
-		}
-
-		else if (vis.save_) {
-			string path_result = path + "Result/";
-			SaveResult(pc_origin, manager, count_move_state, path_result);
-			vis.save_ = false;
-			cout << "#################### Result save finish : " << path_result << endl;
-		}
-		else if (vis.ground_) {
-			string path_result = path + "Result/";
-			int num_graph = manager.out_state_[count_move_state].graph_.size();
-			for (int i = 0; i < SHARD_NUMBER; i++) {
-				Matrix4d T_a, T_obj;
-				Trans T_out;
-				T_axis[i].Output(T_a);
-				T_out.Set(T_a, i, i);
-				for (int j = 0; j < num_graph; j++) {
-					if (manager.out_state_[count_move_state].graph_[j].node_[i]) {
-						manager.out_state_[count_move_state].graph_[j].T_[i].Output(T_obj);
-						T_out.Input(T_obj);
-						T_out.Save(path_result);
-						break;
-					}
-				}
-				
-			}
-			vis.ground_ = false;
-			cout << "#################### Saving transformation matrix over : " << path_result << endl;
-		}
-
-		else if (vis.fine_ && !vis.is_fine_) {
-			cout << "#################### Performing Fine Registration ####################" << endl;
-			vis.is_fine_ = true;
-			TurnoffandReturn(viewer, pc_origin, manager, count_move_state);
-			pc_overlap[count_move_state].TurnOffData(viewer);
-			int num_graph = manager.out_state_[count_move_state].graph_.size();
-			vector<Trans> T_result = T_axis;
-			vector<Matrix3d> R_fine(SHARD_NUMBER);
-			vector<Vector3d> t_fine(SHARD_NUMBER);
-			MatrixXd graph(SHARD_NUMBER, SHARD_NUMBER);
-			for (int i = 0; i < SHARD_NUMBER; i++) {
-				T_fine[i].Set(Matrix4d::Identity());
-				R_fine[i] = Matrix3d::Identity();
-				t_fine[i] = { 0, 0, 0 };
-				for (int j = 0; j < SHARD_NUMBER; j++) {
-					graph(i, j) = 0;
-				}
-			}
-
-			// ########## Restore the solution
-			for (int i = 0; i < num_graph; i++) {
-				for (int j = 0; j < SHARD_NUMBER; j++) {
-					Matrix3d R_prev;
-					Vector3d t_prev;
-					if (manager.out_state_[count_move_state].graph_[i].node_[j]) {
-						manager.out_state_[count_move_state].graph_[i].T_[j].Output(R_prev, t_prev);
-						manager.shard_[j].Move(R_prev, t_prev, true);
-						pc_origin[j].MeshTransform(R_prev, t_prev, viewer);
-						T_result[j].Input(R_prev, t_prev);
-					}
-				}
-				MatrixXd graph_dummy(SHARD_NUMBER, SHARD_NUMBER);
-
-				for (int j = 0; j < SHARD_NUMBER; j++) {
-					if (manager.out_state_[count_move_state].graph_[i].node_[j]) {
-						T_fine[j].Input(R_fine[j], t_fine[j]);
-						T_result[j].Input(R_fine[j], t_fine[j]);
-						Matrix3d R_dummy = Matrix3d::Identity();
-						Vector3d t_dummy = { 0, double(200 * i), 0 };
-						EdgeLineMove(manager.shard_[j].edge_line_, R_dummy, t_dummy);
-
-						T_fine[j].Output(R_dummy, t_dummy);
-						t_dummy[1] = t_dummy[1] + 200 * i;
-						pc_origin[j].MeshTransform(R_dummy, t_dummy, viewer);
-					}
-					for (int k = 0; k < SHARD_NUMBER; k++) {
-						if (graph_dummy(j, k))
-							graph(j, k) = 1;
-					}
-				}
-			}
-			string path_result = path + "Result/";
-			int k_sherd, k_edge, t_sherd, t_edge;
-			tie(k_sherd, t_sherd, k_edge, t_edge)
-				= CountResult(GT_graph, GT_trans,
-					graph, T_result,
-					right_sherd);
-
-			pair<int, int> sherd_acc, edge_acc;
-			sherd_acc = make_pair(k_sherd, t_sherd);
-			edge_acc = make_pair(k_edge, t_edge);
-			SaveAcc(path_result, sherd_acc, edge_acc, time_result);
-
-			for (int j = 0; j < SHARD_NUMBER; j++) {
-				if (manager.out_state_[count_move_state].true_node_[j]) {
-					if (right_sherd[j]) {
-						pc_origin[j].UpdateData(viewer,
-							manager.shard_[j].edge_line_.point_,
-							manager.shard_[j].edge_line_.normal_);
-						pc_origin[j].AddPointCloud(viewer);
-					}
-					else if (!right_sherd[j] && shard_on_off[j]) {
-						pc_origin[j].UpdateData(viewer,
-							manager.shard_[j].edge_line_.point_,
-							manager.shard_[j].edge_line_.normal_,
-							255, 0, 0);
-						pc_origin[j].AddPointCloud(viewer);
-
-					}
-				}
-			}
-			vis.fine_ = false;
-		}
+		viewer->spinOnce(100);
 	}
 
 	return 0;
